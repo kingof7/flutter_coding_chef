46e160653b3f94e6a6ea8f4c01956656a62f5121
kingof7 <vlfl0@naver.com>
2023-02-22 23:37:17 +0900

    2강 - 6분 47초

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+// State가 변하는 조건 2가지
+// Child 위젯의 생성자를 통해서 데이터가 전달 될 때
+// Internal state가 바뀔 때
+// 2강 - 1분37초, 6분 47초
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
