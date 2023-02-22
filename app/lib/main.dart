79027cc783fad264a252e37bf0dda6a3b4d8476c
kingof7 <vlfl0@naver.com>
2023-02-22 23:28:33 +0900

    1| Stateful widget part 1

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,13 +3,33 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
         appBar: AppBar(),
-        body: const Center(
-          child: Text('Coding Chef'),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('You have pushed the button this many times:'),
+              Text(
+                '$counter',
+                style: Theme.of(context).textTheme.displaySmall,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            counter++;
+            print("$counter");
+          },
         ),
       ),
     );
